2ff79a96ba8e94b1f3c762c9588268ef1b25aa9b
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-26 22:10:55 +0530

    A

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 //  Unsplash API
 
-const count = 12;
+const count = 30;
 const apiKey = 'EJY1VGbdFoQWYXimM7wSMH4stzgK9Mi1JpKAK42t2UA';
 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}
 `;
+
+// Check if all images were loaded
+
+function imageLoaded() {
+  console.log('Image loaded');
+  imagesLoaded += 1;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    console.log(' ready = ', ready);
+  }
+}
 
 //  Helper Function to Set Attributes on DOM Element
 
@@ -22,6 +37,8 @@ function setAttributes(element, attributes) {
 // Create Elements for links and Photos, Add to DOM
 
 const displayPhotos = () => {
+  totalImages = photosArray.length;
+  console.log('total images ', totalImages);
   photosArray.forEach((photo) => {
     //Create <a> to link to Unsplash
     const item = document.createElement('a');
@@ -45,6 +62,9 @@ const displayPhotos = () => {
       title: photo.alt_description,
     });
 
+    // Event Listener, Check wether each img is finished loading
+
+    img.addEventListener('load', imageLoaded);
     // Put Image Inside Anchor element and Anchor inside Image container
 
     item.appendChild(img);
@@ -64,6 +84,18 @@ async function getPhotos() {
     // Catch Error
   }
 }
+
+// Check to see if scrolling near bottom of the Page, Load More Photos
+
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.screenY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhotos();
+    console.log('load more');
+  }
+});
 
 //  On Load
 
